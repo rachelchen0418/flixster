d6159a0b8416973b2fc6b3b82056a15fcb3ee357
Rachel Chen <rachelchen0418@gmail.com>
2019-02-28 20:27:10 -0500

    add menu bar, superhero view. grid system

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Rachel Chen on 2/28/19.
+//  Copyright © 2019 rachelchen0418. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
